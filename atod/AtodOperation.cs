776fd1509b32e099b238250a3bc7b4049ba2250a
Christopher - RtF <58520035+christopher-rtf@users.noreply.github.com>
2022-08-15 11:26:49 -0400

    Add 'install' command-line option

---
+++ atod/AtodOperation.cs
@@ -0,0 +1,30 @@
+﻿using Morphic.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atod;
+
+public record AtodOperation : MorphicAssociatedValueEnum<AtodOperation.Values>
+{
+    // enum members
+    public enum Values
+    {
+        Install,
+    }
+
+    // functions to create member instances
+    public static AtodOperation Install(string applicationName, string? fullPath) => new AtodOperation(Values.Install) { ApplicationName = applicationName, FullPath = fullPath };
+
+    // associated values
+    public string? ApplicationName { get; private set; }
+    public string? FullPath { get; private set; }
+
+    // verbatim required constructor implementation for MorphicAssociatedValueEnums
+    private AtodOperation(Values value) : base(value) { }
+}
+
+
--- atod/Program.cs
+++ atod/Program.cs
@@ -15,14 +15,21 @@
 // * Adobe Foundation
 // * Consumer Electronics Association Foundation
 
+using atod;
 using System;
 using System.Reflection;
 
+// print out program header
+//
 var executingAssemblyVersion = Assembly.GetExecutingAssembly().GetName().Version!;
 var versionString = executingAssemblyVersion.Major.ToString() + "." + executingAssemblyVersion.Minor.ToString() + " (build " + executingAssemblyVersion.Build.ToString() + ")";
 //
 var executingAssemblyCopyrightAttribute = Assembly.GetExecutingAssembly().GetCustomAttribute(typeof(AssemblyCopyrightAttribute));
-var copyrightString = executingAssemblyCopyrightAttribute?.ToString();
+string? copyrightString = null;
+if (executingAssemblyCopyrightAttribute is not null)
+{
+    copyrightString = ((AssemblyCopyrightAttribute)executingAssemblyCopyrightAttribute).Copyright;
+}
 //
 Console.WriteLine("AT on Demand v" + versionString);
 if (copyrightString is not null)
@@ -32,3 +39,115 @@ if (copyrightString is not null)
 //
 Console.WriteLine();
 
+var showGeneralHelp = false;
+
+AtodOperation? atodOperationAsNullable = null;
+
+// parse the command-line arguments
+//
+var commandLineArgs = Environment.GetCommandLineArgs();
+if (commandLineArgs is not null)
+{
+    if (commandLineArgs.Length > 1)
+    {
+        var commandArg = commandLineArgs[1]!;
+        switch (commandArg.ToUpperInvariant())
+        {
+            case "INSTALL":
+                {
+                    var showInstallCommandHelp = false;
+
+                    string? applicationName;
+                    if (commandLineArgs.Length > 2)
+                    {
+                        applicationName = commandLineArgs[2];
+
+                        string? fullPath = null;
+                        if (commandLineArgs.Length > 3)
+                        {
+                            fullPath = commandLineArgs[3];
+                        }
+                        else
+                        {
+                            // TODO: if not provided, search for the path to the installer (and remove this block of code)
+                            Console.WriteLine("Sorry, path is required in the current implementation; automatic download will be added in a future release");
+                            Console.WriteLine();
+                            //
+                            showInstallCommandHelp = true;
+                        }
+
+                        atodOperationAsNullable = AtodOperation.Install(applicationName, fullPath);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Application name was not provided.");
+                        Console.WriteLine();
+                        //
+                        showInstallCommandHelp = true;
+                    }
+
+                    if (showInstallCommandHelp == true)
+                    {
+                        // missing application name
+                        Console.WriteLine("Usage:");
+                        Console.WriteLine("  atod install <APPLICATION_NAME> [path]");
+                        Console.WriteLine();
+                        Console.WriteLine("Required arguments:");
+                        Console.WriteLine("  <APPLICATION_NAME>  The name of the application to install.");
+                        Console.WriteLine();
+                        Console.WriteLine("Optional arguments:");
+                        Console.WriteLine("  [path]              The full path to the application installer.");
+                        Console.WriteLine();
+                        return;
+                    }
+                }
+                break;
+            case "HELP":
+                {
+                    showGeneralHelp = true;
+                }
+                break;
+            default:
+                {
+                    // invalid command
+                    Console.WriteLine("Unknown command: " + commandArg);
+                    Console.WriteLine();
+                }
+                //
+                showGeneralHelp = true;
+                break;
+        }
+    }
+    else
+    {
+        showGeneralHelp = true;
+    }
+}
+
+if (atodOperationAsNullable is null || showGeneralHelp == true)
+{
+    Console.WriteLine("Usage:");
+    Console.WriteLine("  atod <command> [arguments]");
+    Console.WriteLine();
+    Console.WriteLine("Commands:");
+    Console.WriteLine("  install        Install an application.");
+    Console.WriteLine();
+    return;
+}
+
+// NOTE: at this point, we know we have a valid operation
+var atodOperation = atodOperationAsNullable!;
+
+// execute the operation
+//
+switch (atodOperation.Value)
+{
+    case AtodOperation.Values.Install:
+        {
+            Console.WriteLine("TODO: trigger installation...");
+        }
+        break;
+    default:
+        throw new NotImplementedException();
+}
+return;
